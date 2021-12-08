fe186c7fd4066c8e624814811512c92c011bc79c
Janit Pathak <janitpathak@gmail.com>
2021-12-08 21:07:59 +0000

    after uncommenting database in settings.py

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -83,37 +83,37 @@ AUTH_USER_MODEL = 'accounts.Account'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 
-# if 'RDS_DB_NAME' in os.environ:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql',
-#             'NAME': os.environ['RDS_DB_NAME'],
-#             'USER': os.environ['RDS_USERNAME'],
-#             'PASSWORD': os.environ['RDS_PASSWORD'],
-#             'HOST': os.environ['RDS_HOSTNAME'],
-#             'PORT': os.environ['RDS_PORT'],
-#         }
-#     }
-# else:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': BASE_DIR / 'db.sqlite3',
-#         }
-#     }
-
-DATABASES = {
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'ebdb',
-            'USER': 'greatkartuser',
-            'PASSWORD': 'asdfghjkl123',
-            # 'USER': 'janit',
-            # 'PASSWORD': 'janit123',
-            'HOST': 'aa1a1lozmtzk6n8.cl0pbpduwcnq.us-east-1.rds.amazonaws.com',
-            'PORT': 5432,
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
         }
     }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
+# DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql',
+#             'NAME': 'ebdb',
+#             'USER': 'greatkartuser',
+#             'PASSWORD': 'asdfghjkl123',
+#             # 'USER': 'janit',
+#             # 'PASSWORD': 'janit123',
+#             'HOST': 'aa1a1lozmtzk6n8.cl0pbpduwcnq.us-east-1.rds.amazonaws.com',
+#             'PORT': 5432,
+#         }
+#     }
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
